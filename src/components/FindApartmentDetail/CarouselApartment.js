a8d83b0f3f9b2e0fa7b4f2a526f1738210f90a7e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-11-21 13:22:09 +0100

    message

--- src/components/FindApartmentDetail/CarouselApartment.js
+++ src/components/FindApartmentDetail/CarouselApartment.js
@@ -1,28 +1,70 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 
-const CarouselApartment = ({images}) => {
-    const [currentImage,setCurrentImage ] = useState(0)
+const CarouselApartment = ({ images }) => {
+  const [currentImage, setCurrentImage] = useState(0);
   return (
-    <div className='relative w-full'>
-    <div className='absolute top-[45%] z-20 py-5 px-1 bg-teal-600 rounded-lg left-0'>
-        <button type='button' onClick={() => setCurrentImage(prevState => prevState === 0 ? prevState : prevState -  1)} className='text-white'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-            </svg>
+    <div className="relative w-full">
+      <div className="absolute top-[45%] z-20 py-5 px-1 bg-teal-600 rounded-lg left-0">
+        <button
+          type="button"
+          onClick={() =>
+            setCurrentImage((prevState) =>
+              prevState === 0 ? prevState : prevState - 1
+            )
+          }
+          className="text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5L8.25 12l7.5-7.5"
+            />
+          </svg>
         </button>
-    </div>
-    <div className="carousel-inner h-[500px] relative w-full overflow-hidden rounded-lg">
-        <img className='h-[600px] w-full ' src={images[currentImage]}/>
-    </div>
-    <div className='absolute top-[45%] z-20 py-5 px-1 bg-teal-600 rounded-lg right-0'>
-        <button type='button' onClick={() => setCurrentImage(prevState => prevState === images.length - 1 ? prevState : prevState + 1 )} className='text-white'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </svg>
+      </div>
+      <div className="carousel-inner h-[500px] relative w-full overflow-hidden rounded-lg">
+        <img
+          className="h-[600px] w-full "
+          src={images[currentImage]}
+          alt="current"
+        />
+      </div>
+      <div className="absolute top-[45%] z-20 py-5 px-1 bg-teal-600 rounded-lg right-0">
+        <button
+          type="button"
+          onClick={() =>
+            setCurrentImage((prevState) =>
+              prevState === images.length - 1 ? prevState : prevState + 1
+            )
+          }
+          className="text-white"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 4.5l7.5 7.5-7.5 7.5"
+            />
+          </svg>
         </button>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default CarouselApartment
+export default CarouselApartment;
