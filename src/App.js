7017a200c18e70e913da66d6b8a1e540a9785abe
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-11-21 23:18:07 +0100

    message

--- src/App.js
+++ src/App.js
@@ -17,6 +17,11 @@ import Aboutus from "./pages/Aboutus";
 import FindApartment from "./pages/FindApartment";
 import FindApartmentDetail from "./pages/FindApartmentDetail";
 import Modal from "./components/FindApartmentDetail/Modal";
+import ToastifyComponent from "./context/ToastifyContext";
+import UserComponent from "./context/UserContext";
+import Toastify from "./components/Toastify";
+import Signup from "./admin/SignUp";
+import Signin from "./admin/Signin";
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -34,45 +39,56 @@ function App() {
 
   return (
     <>
-      <div className={`${showModal ? " h-screen overflow-hidden" : ""}`}>
-        <BrowserRouter>
-          {showModal && (
-            <Modal
-              closeModal={closeModal}
-              openModal={openModal}
-              modalStage={modalStage}
-            />
-          )}
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/about-us" element={<Aboutus />} />
-            <Route
-              exact
-              path="/find-apartment/search"
-              element={<FindApartmentSearch />}
-            />
-            <Route
-              exact
-              path="/find-apartment/detail"
-              element={
-                <FindApartmentDetail
+      <UserComponent>
+        <ToastifyComponent>
+          <Toastify />
+          <div className={`${showModal ? " h-screen overflow-hidden" : ""}`}>
+            <BrowserRouter>
+              {showModal && (
+                <Modal
                   closeModal={closeModal}
                   openModal={openModal}
+                  modalStage={modalStage}
                 />
-              }
-            />
-            <Route exact path="/find-apartment" element={<FindApartment />} />
-            <Route exact path="/recoa-communities" element={<Login />} />
-            <Route
-              exact
-              path="/corporate-tenant"
-              element={<>corporate tenant</>}
-            />
-          </Routes>
-        </BrowserRouter>
-      </div>
+              )}
+              <Navbar />
+              <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/about-us" element={<Aboutus />} />
+                <Route
+                  exact
+                  path="/find-apartment/search"
+                  element={<FindApartmentSearch />}
+                />
+                <Route
+                  exact
+                  path="/find-apartment/detail"
+                  element={
+                    <FindApartmentDetail
+                      closeModal={closeModal}
+                      openModal={openModal}
+                    />
+                  }
+                />
+                <Route
+                  exact
+                  path="/find-apartment"
+                  element={<FindApartment />}
+                />
+                <Route exact path="/recoa-communities" element={<Login />} />
+                <Route
+                  exact
+                  path="/corporate-tenant"
+                  element={<>corporate tenant</>}
+                />
+                <Route path="/admin_signup" element={<Signup />} />
+                <Route path="/admin_signin" element={<Signin />} />
+              </Routes>
+            </BrowserRouter>
+          </div>
+        </ToastifyComponent>
+      </UserComponent>
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,63 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import Background_image from '../images/Rectangle 46 (1).jpg'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Background_image from "../images/Rectangle 46 (1).jpg";
 
 const Login = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className='h-screen relative '>
-    <div className='w-full h-full absolute blur  contrast-[0.4]' style={{
-    backgroundImage: `url("${Background_image}")` , 
-    backgroundSize: 'cover'}} ></div>
-    <div className='w-full h-full z-10 relative flex flex-row justify-center items-center'>
-        <div className='content flex flex-col items-center -mt-20'>
-            <div className='text-6xl mb-3 font-extrabold text-red-900'>
-                 ExxonMobil
-            </div> 
-          
-            
-            <div className='flex bg-white justify-between items-center py-3 px-4 rounded-lg w-full mt-5'>
-                <input className='appearance-none text-xl' name='password' placeholder='Enter Password' type='password'/>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
-                    </svg>
-                </span>
-            </div>
-            
-            <div className='relative mt-10 flex flex-row justify-center'>
-                <button onClick={() => navigate('/find-apartment/search')} className='px-12 py-2 border-4 text-xl text-white border-white rounded-full uppercase' type='button'> Login </button>
-            </div>
-        </div>
-    </div>
-</div>
-  )
-}
+    <div className="h-screen relative ">
+      <div
+        className="w-full h-full absolute blur  contrast-[0.4]"
+        style={{
+          backgroundImage: `url("${Background_image}")`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <div className="w-full h-full z-10 relative flex flex-row justify-center items-center">
+        <div className="content flex flex-col items-center -mt-20">
+          <div className="text-6xl mb-3 font-extrabold text-red-900">
+            ExxonMobil
+          </div>
 
-export default Login
+          <div className="flex bg-white justify-between items-center py-3 px-4 rounded-lg w-full mt-5">
+            <input
+              className="appearance-none text-xl"
+              name="password"
+              placeholder="Enter Password"
+              type="password"
+            />
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                />
+              </svg>
+            </span>
+          </div>
+
+          <div className="relative mt-10 flex flex-row justify-center">
+            <button
+              onClick={() => navigate("/find-apartment/search")}
+              className="px-12 py-2 border-4 text-xl text-white border-white rounded-full uppercase"
+              type="button"
+            >
+              {" "}
+              Login{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
